b6614cb4ff6a98364c427239ed9280fa3fa8e5c8
Filip Furár <furar@stud.uniza.sk>
2025-06-03 19:14:26 +0200

    add other files to git

---
+++ app/src/main/java/com/example/gamascheduler/login/LoginScreen.kt
@@ -0,0 +1,52 @@
+package com.example.gamascheduler.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.Text
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.gamascheduler.R
+import androidx.lifecycle.ViewModel
+
+@Composable
+fun LoginScreen() {
+
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
+    Column (
+        verticalArrangement = Arrangement.spacedBy(mediumPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(mediumPadding)
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            textAlign = TextAlign.Center,
+        )
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            onValueChange = {},
+            label = { Text(stringResource(R.string.username)) },
+            isError = false,
+            keyboardActions = KeyboardActions(
+                onDone = {}
+            )
+        )
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            onValueChange = {},
+            label = { Text(stringResource(R.string.password)) },
+            isError = false,
+            keyboardActions = KeyboardActions(
+                onDone = {}
+            )
+        )
+    }
+}
--- app/src/main/java/com/example/gamascheduler/login/LoginViewModel.kt
+++ app/src/main/java/com/example/gamascheduler/login/LoginViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.gamascheduler.login
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class LoginUIState(
+    val currentUsername: String = "",
+    val currentPassword: String = ""
+)
+
+class LoginViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUIState())
+    val uiState: StateFlow<LoginUIState> = _uiState.asStateFlow()
+
+
+}
